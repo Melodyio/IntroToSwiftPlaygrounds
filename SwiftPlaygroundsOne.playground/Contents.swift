033544991a4188bb1e8c361e21bf4aef8e1f72f1
Melody <melodyio@hawaii.edu>
2018-01-25 20:34:04 -1000

    finished Swift Playfrounds One Assignment

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -2,7 +2,16 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var myFirstVariable: String
+myFirstVariable = "I've created my first variable"
+
+//We're going to do something with strings
+var mySecondVariable = "and look I added two strings together"
+
+
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+print(myThirdVariable)
 
 // Assignment #2 Introduction to True and False
 var appState = false
